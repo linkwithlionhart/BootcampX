20c1657337d6065be00ff2b6f62aeeb84c484a8c
linkwithlionhart <linkwithlionhart@gmail.com>
2023-09-24 01:40:30 -0700

    Implement last round of queries

---
+++ 4_queries/assistance_requests_data.sql
@@ -0,0 +1,9 @@
+SELECT teachers.name as teacher,
+  students.name as student,
+  assignments.name as assignment,
+  (completed_at - started_at) as duration
+FROM assistance_requests
+LEFT JOIN teachers ON teachers.id = assistance_requests.teacher_id
+LEFT JOIN students ON students.id = assistance_requests.student_id
+LEFT JOIN assignments ON assignments.id = assistance_requests.assignment_id
+ORDER BY duration;
